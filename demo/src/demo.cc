4793ca72dcd1ab4c2698448388a513ad2d39558a
Kai Zhao <loverszhao@gmail.com>
2015-08-30 19:09:44 +0800

    Update demo

---
+++ demo/src/demo.cc
@@ -0,0 +1,23 @@
+/*
+ * Author   : Kai Zhao
+ * Email    : loverszhao@gmail.com
+ * Reference:
+ */
+
+/*
+ * Description:
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "===================================" << endl;
+	cout << "\tThis is a demo project" << endl;
+	cout << "===================================" << endl;
+
+	return 0;
+}
